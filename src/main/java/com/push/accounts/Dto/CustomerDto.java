47c32cd184e0407ec2a08c0b9d6dfe820404e2c3
Pushparaj A <73117681+pushparaj1234@users.noreply.github.com>
2026-07-26 00:13:37 +0530

    add input validations (#4)
    * add entity,DTO for accounts and customer
    
    * Add controller and methods to create account for a customer
    
    * Created post, put and delete endpoints with error handling
    
    * Add validations

--- src/main/java/com/push/accounts/Dto/CustomerDto.java
+++ src/main/java/com/push/accounts/Dto/CustomerDto.java
@@ -1,12 +1,20 @@
 package com.push.accounts.Dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
-
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min=5, max=30, message="The length of the customer should be from 5 to 30")
     private String name;
+    @NotEmpty(message = "email cannot be empty")
+    @Email(message = "Email address should be in valid format")
     private String email;
+    @NotEmpty(message = "mobileNumber cannot be empty")
+    @Pattern(regexp = "^$|[0-9]{10}", message = "The mobile number must be 10 digits")
     private String mobileNumber;
+    @Valid
     private AccountsDto accountsDto;
 }
--- src/main/java/com/push/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/push/accounts/exception/GlobalExceptionHandler.java
@@ -1,17 +1,47 @@
 package com.push.accounts.exception;
 
 import com.push.accounts.Dto.ErrorResponseDto;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.lang.model.type.ErrorType;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    /**
+     * Customize the handling of {@link MethodArgumentNotValidException}.
+     * <p>This method delegates to {@link #handleExceptionInternal}.
+     *
+     * @param ex      the exception to handle
+     * @param headers the headers to be written to the response
+     * @param status  the selected response status
+     * @param request the current request
+     * @return a {@code ResponseEntity} for the response to use, possibly
+     * {@code null} when the response is already committed
+     */
+    @Override
+    protected @Nullable ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,String> validationErrors = new HashMap<>();
+        for (FieldError errors : ex.getBindingResult().getFieldErrors()) {
+            validationErrors.put(errors.getField(), errors.getDefaultMessage());
+        }
+        return new ResponseEntity<>(validationErrors,HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception e, WebRequest webRequest) {
